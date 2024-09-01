dc291b7ec3f9bd7899de95dfff594c50c5f46d5d
Tim Stirrat <tim.stirrat@gmail.com>
2024-09-01 10:04:14 +1000

    Hide the sysex behind details, use Button

--- src/components/SysexPreview.tsx
+++ src/components/SysexPreview.tsx
@@ -1,8 +1,9 @@
 import { Card } from "primereact/card";
 import { Waveform } from "../types";
 import { sysexWaveformMessage, toHex } from "../lib/sysex";
-import { Flex } from "./Flex";
 import { useMemo } from "react";
+import { Button } from "primereact/button";
+import { PrimeIcons } from "primereact/api";
 
 export const SysexPreview: React.FC<{ waveform: Waveform }> = ({
   waveform,
@@ -33,16 +34,35 @@ ${sysex[23]} = SYSEX EOF
     return URL.createObjectURL(blobConfig);
   }, [waveform]);
 
+  const handleDownload = () => {
+    const fileName = "mGB-patch.syx";
+    downloadFile(blobUrl, fileName);
+  };
+
   return (
-    <Card title="Sysex data">
-      <code>
-        <pre>{output}</pre>
-      </code>
-      <Flex row align="end" grow="1">
-        <a href={blobUrl} download="mGB-patch.syx">
-          Download .syx file
-        </a>
-      </Flex>
+    <Card>
+      <Button
+        icon={PrimeIcons.DOWNLOAD}
+        onClick={handleDownload}
+        label="Download .syx file"
+      />
+      <details>
+        <summary>
+          <strong>Sysex data</strong>
+        </summary>
+        <code>
+          <pre>{output}</pre>
+        </code>
+      </details>
     </Card>
   );
 };
+
+function downloadFile(blobUrl: string, fileName: string) {
+  const link = document.createElement("a");
+  link.href = blobUrl;
+  link.download = fileName;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
